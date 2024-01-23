63e4922c1ef6f012bfbb56ac0efa06f07a829168
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-23 20:19:16 +0700

    second

--- calculating_income_tax.py
+++ calculating_income_tax.py
@@ -5,8 +5,17 @@ def calculate_income_tax(taxable_income):
     rate3 = 0.20
     threshold1 = 10000
     threshold2 = 20000
-    
+
 # Calculate income tax
+    if taxable_income <= threshold1:
+        income_tax = taxable_income * rate1
+    elif threshold1 < taxable_income <= threshold2:
+        income_tax = (threshold1 * rate1) + ((taxable_income - threshold1) * rate2)
+    else:
+        income_tax = (threshold1 * rate1) + (threshold2 * rate2) + ((taxable_income - threshold2) * rate3)
+
+    return income_tax
 # Test case
+
 # Print the result
 
